1ad2743cbb614cc1ea8bf811930d225595726dbd
Jean Carlos <jeancarlosaps@yahoo.com.br>
2017-06-23 00:48:30 -0300

    Adding Uber API parameters and separating responsibilities

---
+++ dividepranos/dividepranos/Class/SplitAccount.swift
@@ -0,0 +1,22 @@
+//
+//  SplitAccount.swift
+//  dividepranos
+//
+//  Created by Jean Carlos on 6/23/17.
+//  Copyright © 2017 Jean Carlos. All rights reserved.
+//
+
+import Foundation
+
+func SplitAccount(Float: numberOfPeople, Float: accountAmount, Float: extras, Float: serviceCharge) -> Float: result{
+//    self.numberOfPeople = Float(self.txfNumberOfPeople.text!)!
+//    self.accountAmount = Float(self.txfAccountAmount.text!)!
+//    self.extras = Float(self.txfExtras.text!)!
+//    self.serviceCharge = Float(self.txfServiceCharge.text!)!
+//    
+//    let result = self.accountAmount + self.extras + self.serviceCharge / self.numberOfPeople
+//    
+//    self.lblAccountResult.text = String(result)
+    
+    return result = 0.0
+}
